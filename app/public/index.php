02dcda3e9a9a650d1a5d732aa7ba8ddfd9f9dd48
Abu Muhammad <b.hermianto@gmail.com>
2024-09-17 14:28:48 +0700

    Tambahan timezone dan sembunyikan noise error

--- app/public/index.php
+++ app/public/index.php
@@ -1,11 +1,13 @@
 <?php
+date_default_timezone_set('Asia/Jakarta');
+error_reporting(E_ALL & ~E_DEPRECATED & ~E_NOTICE);
 
 // change the following paths if necessary
 $yii = dirname(__FILE__).'/../vendor/yiisoft/yii/framework/yii.php';
 $config = dirname(__FILE__).'/../src/protected/config/main.php';
 
 // remove the following lines when in production mode
-defined('YII_DEBUG') or define('YII_DEBUG', true);
+// defined('YII_DEBUG') or define('YII_DEBUG', true);
 // specify how many levels of call stack should be shown in each log message
 defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL', 3);
 
