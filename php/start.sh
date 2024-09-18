50a131c6e43d322c7b3c0abf420ad811ff4e8527
Abu Muhammad <b.hermianto@gmail.com>
2024-09-18 10:25:40 +0700

    Composer install pindah ke start.sh

--- php/start.sh
+++ php/start.sh
@@ -5,4 +5,6 @@ nohup php-fpm > /var/log/php-fpm.log 2>&1 &
 # Start the WebSocket server
 nohup php /var/www/html/src/protected/ws/ws_server.php > /var/log/ws_server.log 2>&1 &
 
+composer install --prefer-dist --no-dev --no-scripts --no-progress --no-interaction
+composer dump-autoload --optimize
 wait
