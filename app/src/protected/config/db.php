6985461851b5380f26d174765bca49d20a074085
Abu Muhammad <b.hermianto@gmail.com>
2024-09-18 10:08:04 +0700

    DB Config: Update to variable

--- app/src/protected/config/db.php
+++ app/src/protected/config/db.php
@@ -1,10 +1,10 @@
 <?php
 
 return [
-  'connectionString'   => 'mysql:host='.getenv('DOCKERHOSTIP').';dbname=ahadpos3;port=3307',
+  'connectionString'   => 'mysql:host=' . getenv('DOCKERHOSTIP') . ';dbname=' . getenv('MYSQL_DATABASE') . ';port=' . getenv('MYSQL_PORT'),
   'emulatePrepare'     => true,
-  'username'           => 'admin',
-  'password'           => 'adminpassword',
+  'username'           => getenv('MYSQL_USER'),
+  'password'           => getenv('MYSQL_PASSWORD'),
   'charset'            => 'utf8',
   'enableParamLogging' => true,
   // 'pdoClass' => 'NestedPDO',
