71c6a08f5dd5c6c6e17a604566efb3ed29f7561a
Abu Muhammad <b.hermianto@gmail.com>
2024-09-18 11:37:04 +0700

    update dockerhostip

--- dockerhostip.sh
+++ dockerhostip.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 export DOCKERHOSTIP=$(route -n | awk '/UG[ \t]/{print $2}')
-export CONTAINERIP=$(hostname -I | awk '{print $1}')
-echo "Docker Host IP: $DOCKERHOSTIP"
+# echo "Docker Host IP: $DOCKERHOSTIP"
 exec "$@"
